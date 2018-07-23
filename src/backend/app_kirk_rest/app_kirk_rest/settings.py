c135805968e8cf22e676a9c97b85bb9d3aa88aea
GuyLafleur <kevin.netherton@gov.bc.ca>
2018-07-23 16:46:20 -0700

    settings config

--- src/backend/app_kirk_rest/app_kirk_rest/settings.py
+++ src/backend/app_kirk_rest/app_kirk_rest/settings.py
@@ -23,7 +23,8 @@ FIXTURE_DIRS = (os.path.join(PROJECT_ROOT, 'fixtures'),)
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@f95&sb4(u#ek9edzdn)^_f4t&vo!dr3s0z&0sdm06t3&^r(5d'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+#SECRET_KEY = '@f95&sb4(u#ek9edzdn)^_f4t&vo!dr3s0z&0sdm06t3&^r(5d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
