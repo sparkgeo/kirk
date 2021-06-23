6b7484418a319b1c0d4eace480cfa10f57a79c94
Yuhan Freeman <yhfreeman12@gmail.com>
2021-06-23 15:13:55 -0700

    Library upgrades (#69)
    * resolve conflict for lib upgrade
    
    * bump release minor

--- src/backend/app_kirk_rest/app_kirk_rest/settings.py
+++ src/backend/app_kirk_rest/app_kirk_rest/settings.py
@@ -162,5 +162,6 @@ REST_FRAMEWORK = {
         #'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
